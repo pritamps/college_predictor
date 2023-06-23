81cd863d6712b0bc7b0ed721d364ad8841e088d7
suryabulusu <teja.surya59@gmail.com>
2023-06-23 18:52:01 +0530

    adds round info

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import styles from "./index.module.css";
 
 const HomePage = () => {
-    const options = [
+    const categoryOptions = [
         { value: "ews_pwd", label: "EWS (PwD)" },
         { value: "ews", label: "EWS" },
         { value: "obc_ncl_pwd", label: "OBC-NCL (PwD)" },
@@ -17,13 +17,27 @@ const HomePage = () => {
         { value: "st", label: "ST" },
     ];
 
+    const roundNumberOptions = [
+        { value: 1, label: "Round 1" },
+        { value: 2, label: "Round 2" },
+        { value: 3, label: "Round 3" },
+        { value: 4, label: "Round 4" },
+        { value: 5, label: "Round 5" },
+        { value: 6, label: "Round 6" }
+    ];
+
     const [rank, setRank] = useState(0);
+    const [roundNumber, setRoundNumber] = useState("");
     const [category, setCategory] = useState("");
     const router = useRouter();
 
-    const handleDropdownChange = (selectedOption) => {
-        console.log("Selected option:", selectedOption);
-        setCategory(selectedOption.label);
+    const handleDropdownChange = (selectedOption, dropdownType) => {
+        if (dropdownType == "category") {
+            setCategory(selectedOption.label);
+        }
+        else if (dropdownType == "roundNumber") {
+            setRoundNumber(selectedOption.value);
+        }
     };
 
     const handleRankChange = (event) => {
@@ -32,25 +46,39 @@ const HomePage = () => {
     };
 
     const handleSubmit = () => {
-        router.push(`/college_predictor?rank=${rank}&category=${category}`);
+        router.push(`/college_predictor?rank=${rank}&category=${category}&roundNumber=${roundNumber}`);
     };
 
-    const isSubmitDisabled = rank <= 0 || category === "";
+
+    const isCategoryInOptions = categoryOptions.some(option => option.label === category);
+    const isRoundNumberInOptions = roundNumberOptions.some(option => option.value == roundNumber);
+    const isSubmitDisabled = rank <= 0 || !isCategoryInOptions || !isRoundNumberInOptions;
 
     return (
         <div className={styles.container}>
             <div className={styles.content}>
                 <h1>AF College Predictor</h1>
                 <label className={styles.label}>Select Category:</label>
-                <Dropdown options={options} onChange={handleDropdownChange} />
+                <Dropdown 
+                    options={categoryOptions} 
+                    onChange={(selectedOption) => handleDropdownChange(selectedOption, "category")}
+                />
                 <p />
                 <p />
-                <label className={styles.label}>Enter Rank:</label>
+                <label className={styles.label}>Enter Category Rank:</label>
                 <input
                     type="number"
                     value={rank}
                     onChange={handleRankChange}
                     className={styles.input}
+                />
+                <p />
+                <p />
+
+                <label className={styles.label}>Select Round Number:</label>
+                <Dropdown 
+                    options={roundNumberOptions} 
+                    onChange={(selectedOption) => handleDropdownChange(selectedOption, "roundNumber")}
                 />
 
                 <button
