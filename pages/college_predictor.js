a1a42a5bda2587561b4c3a8989dc208ca78fbea3
Pritam Sukumar <pritamps@gmail.com>
2023-06-24 13:43:17 +0530

    Added explanation of quotas

--- pages/college_predictor.js
+++ pages/college_predictor.js
@@ -23,10 +23,12 @@ const CollegePredictor = () => {
                 });
 
                 // Filter the data based on closing rank
-                const filteredData = dataForGivenRoundAndGender.filter((item) => {
-                    const closingRank = parseInt(item["Closing Rank"], 10);
-                    return closingRank > parseInt(rank, 10);
-                });
+                const filteredData = dataForGivenRoundAndGender.filter(
+                    (item) => {
+                        const closingRank = parseInt(item["Closing Rank"], 10);
+                        return closingRank > parseInt(rank, 10);
+                    }
+                );
 
                 // Sort the filteredData in ascending order of college rank
                 filteredData.sort((a, b) => {
@@ -54,8 +56,9 @@ const CollegePredictor = () => {
             <div className={styles.content}>
                 <h1>College Predictor</h1>
                 <h2>Your Category Rank: {rank}</h2>
-                <h2>Chosen Round Number: {roundNumber}</h2>
-                <h2>Chosen Gender: {gender}</h2>
+                <h3>Chosen Round Number: {roundNumber}</h3>
+                <h3>Chosen Gender: {gender}</h3>
+
                 <h3>Predicted colleges and courses for you</h3>
                 {isLoading ? (
                     <div className={styles.loading}>
@@ -63,49 +66,56 @@ const CollegePredictor = () => {
                         <p>Loading...</p>
                     </div>
                 ) : (
-                    <table className={styles.table}>
-                        <thead>
-                            <tr className={styles.header_row}>
-                                <th>Institute Rank</th>
-                                <th>Institute</th>
-                                <th>Academic Program Name</th>
-                                <th>Opening Rank</th>
-                                <th>Closing Rank</th>
-                                <th>Quota</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {filteredData.map((item, index) => (
-                                <tr
-                                    key={index}
-                                    className={
-                                        index % 2 === 0
-                                            ? styles.even_row
-                                            : styles.odd_row
-                                    }
-                                >
-                                    <td className={styles.cell}>
-                                        {item["College Rank"]}
-                                    </td>
-                                    <td className={styles.cell}>
-                                        {item.Institute}
-                                    </td>
-                                    <td className={styles.cell}>
-                                        {item["Academic Program Name"]}
-                                    </td>
-                                    <td className={styles.cell}>
-                                        {item["Opening Rank"]}
-                                    </td>
-                                    <td className={styles.cell}>
-                                        {item["Closing Rank"]}
-                                    </td>
-                                    <td className={styles.cell}>
-                                        {item["Quota"]}
-                                    </td>
+                    <div>
+                        <div>
+                            <p className={styles.paragraph}>AI: All India</p>
+                            <p className={styles.paragraph}>HS: Home State </p>
+                            <p className={styles.paragraph}>OS: Out of State</p>
+                        </div>
+                        <table className={styles.table}>
+                            <thead>
+                                <tr className={styles.header_row}>
+                                    <th>Institute Rank</th>
+                                    <th>Institute</th>
+                                    <th>Academic Program Name</th>
+                                    <th>Opening Rank</th>
+                                    <th>Closing Rank</th>
+                                    <th>Quota</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {filteredData.map((item, index) => (
+                                    <tr
+                                        key={index}
+                                        className={
+                                            index % 2 === 0
+                                                ? styles.even_row
+                                                : styles.odd_row
+                                        }
+                                    >
+                                        <td className={styles.cell}>
+                                            {item["College Rank"]}
+                                        </td>
+                                        <td className={styles.cell}>
+                                            {item.Institute}
+                                        </td>
+                                        <td className={styles.cell}>
+                                            {item["Academic Program Name"]}
+                                        </td>
+                                        <td className={styles.cell}>
+                                            {item["Opening Rank"]}
+                                        </td>
+                                        <td className={styles.cell}>
+                                            {item["Closing Rank"]}
+                                        </td>
+                                        <td className={styles.cell}>
+                                            {item["Quota"]}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 )}
             </div>
         </div>
